5d964447ba922493f28292a41bcac23213643d3f
ALIGregoire <aligregoire8@gmail.com>
2026-05-16 08:44:48 +0000

    first commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestionProduitsMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
