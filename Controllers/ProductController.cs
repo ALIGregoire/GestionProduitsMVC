c486665188ff215bceb02213a575528ccfc6289b
PANIZIM Pélagie <pelagiepanizim@gmail.com>
2026-05-19 17:32:21 +0000

    listeproduits

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,17 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using GestionProduitsMVC.Models;
+using System.Collections.Generic;
 
 namespace GestionProduitsMVC.Controllers
 {
     public class ProductController : Controller
     {
+        private static List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Ordinateur", Price = 250000M, Stock = 5 },
+            new Product { Id = 2, Name = "Téléphone", Price = 150000M, Stock = 10 }
+        };
+
         public IActionResult Index()
         {
-            return View();
+            return View(_products);
         }
 
-        public IActionResult Details()
+        public IActionResult Details(int id)
         {
-            return View();
+            var product = _products.Find(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
         }
 
         public IActionResult Create()
